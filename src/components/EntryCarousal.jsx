2442bf9077662db26972eb82ede43e360be72b55
Shishir Kushwaha <kushwahashishir1112@gmail.com>
2024-02-03 21:33:16 +0530

    Change Navbar Style and Added Background

--- src/components/EntryCarousal.jsx
+++ src/components/EntryCarousal.jsx
@@ -1,9 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react'
-
+import bgImage from '../origin.jpg'
 export default function EntryCarousal() {
     return (
         <ChakraProvider>
-        <div className="flex justify-center items-center h-full my-32">
+        <div className="flex justify-center items-center h-screen bg-cover text-white" style={{backgroundImage:`url(${bgImage})`}}>
             <div className="flex flex-col items-center">
                 <h1 className="text-xl font-bold w-2/4 text-center">Empowering Wellness, Ensuring Privacy: Blockchain-Powered Health Consultations, </h1>
                 <h1 className="text-4xl font-extrabold w-2/4 text-center"> Your <span className="text-blue-600">Health</span>,</h1>
