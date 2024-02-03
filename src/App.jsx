78c5e40b151d1a5e381558b94cd72297a8bfd79c
kmtGryffindor20 <tripathikmt20@gmail.com>
2024-02-03 19:32:15 +0530

    trying

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import EntryCarousal from './components/EntryCarousal'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <ChakraProvider>
       <Navbar />
-      
+      <EntryCarousal />
+    </ChakraProvider>
     </>
   )
 }
