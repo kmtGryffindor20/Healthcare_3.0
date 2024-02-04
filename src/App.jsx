0b7bbe30bd33fb282f7caa7f10a5fbf8ca0b5e9c
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2024-02-04 09:31:44 +0530

    connect to metamask

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import EntryCarousal from './components/EntryCarousal'
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Chat from './Pages/Chat'
-import Footer from './components/Footer'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import EntryCarousal from "./components/EntryCarousal";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Chat from "./Pages/Chat";
+import Footer from "./components/Footer";
 
 function App() {
+  const [haveMetamask, setHaveMetamask] = useState(true);
 
+  useEffect(() => {
+    const { ethereum } = window;
+    const checkMetamaskAvailability = async () => {
+      if (!ethereum) {
+        setHaveMetamask(false);
+      } else {
+        setHaveMetamask(true);
+      }
+    };
+    checkMetamaskAvailability();
+  }, []);
   return (
     <>
-    <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>} exact/>
-          <Route path="/chat" element={<Chat/>} exact/>
-        </Routes>
-        <Footer />
-      </Router>
-    </ChakraProvider>
+      <ChakraProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/chat" element={<Chat />} exact />
+          </Routes>
+          <Footer />
+        </Router>
+      </ChakraProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
